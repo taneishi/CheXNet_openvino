7fac33f95e4d7ad8c8c2f7350d919d62409eb7f4
taneishi <taneishi@users.noreply.github.com>
2021-04-14 17:59:10 +0900

    merge fp32 and int8 run scripts.

--- inference.sh
+++ inference.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=2
-#PBS -N chexnet_int8
+#PBS -N chexnet_run
 #PBS -j oe
-#PBS -o log/output_int8.log
+#PBS -o log/output_inference.log
 
 if [ $(which python) ]; then PYTHON=python; fi
 if [ $(which python3) ]; then PYTHON=python3; fi
 
 if [ ${PBS_O_WORKDIR} ]; then cd ${PBS_O_WORKDIR}; fi
 
+${PYTHON} ov-inference.py --fp32
 ${PYTHON} ov-inference.py --int8
