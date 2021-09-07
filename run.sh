8b9b69b6ae8b8b83c235d3686e6b2bf7455ecf7d
taneishi <taneishi@users.noreply.github.com>
2021-09-07 13:54:32 +0900

    removed CLI model converter and acucuracy checker.

--- run.sh
+++ run.sh
@@ -15,13 +15,7 @@ if [ ${PBS_O_WORKDIR} ]; then cd ${PBS_O_WORKDIR}; fi
 #mkdir -p annotations
 #${PYTHON} annotation.py chest_xray --annotation_file labels/val_list.txt -ss 320 -o annotations -a chestx.pickle -m chestx.json --data_dir images
 
-# accuracy check
-#accuracy_check -c config/chexnet.yaml -m model
-
 # int8 quantization
 #pot -c config/chexnet_int8_pot.yaml -e
-
-# accuracy check
-#accuracy_check -c config/chexnet_int8.yaml -m model
 
 ${PYTHON} ov-inference.py --int8 --batch_size 2048
