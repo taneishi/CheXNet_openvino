dbfff4dbce5f8f2a88cec5f347a7160b864f8112
taneishi <taneishi@users.noreply.github.com>
2021-10-27 13:14:16 +0900

    changed to use venv openvino.

--- run.sh
+++ run.sh
@@ -4,18 +4,23 @@
 #PBS -j oe
 #PBS -o output.log
 
-PYTHON=python3
+PYTHON=python
 
 if [ ${PBS_O_WORKDIR} ]; then cd ${PBS_O_WORKDIR}; fi
 
+source ~/openvino/bin/activate
+
+${PYTHON} export_onnx.py
+
 # fp32 model optimization
 #${PYTHON} ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer/mo.py --input_model ${PWD}/model/densenet121.onnx --output_dir model
+mo --input_model ${PWD}/model/densenet121.onnx --output_dir model
 
 # make annotations
-#mkdir -p annotations
-#${PYTHON} annotation.py chest_xray --annotation_file labels/val_list.txt -ss 320 -o annotations -a chestx.pickle -m chestx.json --data_dir images
+mkdir -p annotations
+${PYTHON} annotation.py chest_xray --annotation_file labels/val_list.txt -ss 320 -o annotations -a chestx.pickle -m chestx.json --data_dir images
 
 # int8 quantization
-#pot -c config/chexnet_int8_pot.yaml -e
+pot -c config/chexnet_int8_pot.yaml -e
 
 ${PYTHON} ov-inference.py --int8 --batch_size 2048
