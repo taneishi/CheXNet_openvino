bf05104623229daf923b1de6702fa17d6c206df6
taneishi <taneishi@users.noreply.github.com>
2021-10-28 10:11:11 +0900

    updated for new int8 config.

--- run.sh
+++ run.sh
@@ -20,6 +20,6 @@ mkdir -p annotations
 ${PYTHON} annotation.py chest_xray --annotation_file labels/val_list.txt -ss 320 -o annotations -a chestx.pickle -m chestx.json --data_dir images
 
 # int8 quantization
-pot -c config/chexnet_int8_pot.yaml -e
+pot -c config/chexnet_int8.yaml -e
 
-${PYTHON} ov-inference.py --int8 --batch_size 2048
+${PYTHON} ov-infer.py --int8
