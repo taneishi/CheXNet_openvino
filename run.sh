b104ec11e0d2859844dfd022791c8754e448f7fe
taneishi <taneishi@users.noreply.github.com>
2021-10-31 12:33:49 +0900

    changed devcloud run script for updated infer.py.

--- run.sh
+++ run.sh
@@ -10,16 +10,6 @@ if [ ${PBS_O_WORKDIR} ]; then cd ${PBS_O_WORKDIR}; fi
 
 source ~/openvino/bin/activate
 
-${PYTHON} export_onnx.py
+${PYTHON} infer.py --fp32
 
-# fp32 model optimization
-mo --input_model ${PWD}/model/densenet121.onnx --output_dir model
-
-# make annotations
-mkdir -p annotations
-${PYTHON} annotation.py chest_xray --annotation_file labels/val_list.txt -ss 320 -o annotations -a chestx.pickle -m chestx.json --data_dir images
-
-# int8 quantization
-pot -c config/chexnet_int8.yaml -e
-
-${PYTHON} ov-infer.py --int8
+${PYTHON} infer.py --int8
