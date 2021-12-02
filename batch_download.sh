120ac943c8bcd8f2138568cd88321eec40469319
taneishi <taneishi@users.noreply.github.com>
2021-12-02 12:06:59 +0900

    changed download method to wget.

---
+++ batch_download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Download the 12 tgz files in batches
+
+index=1
+for link in $(cat urls.txt) # URLs for the tar.gz files
+do
+    seq=$(printf "%02d" ${index})
+    fn="rmages_png_${seq}.tar.gz"
+    echo "wget -c -O ${fn} ${link}"
+    wget -c -O ${fn} ${link}
+    index=$((index+1))
+done
+
+echo "Download complete. Please check the checksums"
+sha256sum -c SHA256_checksums.txt
